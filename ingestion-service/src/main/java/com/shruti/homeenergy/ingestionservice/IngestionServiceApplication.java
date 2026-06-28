1f22e2df14d18c2285de02286673ab340022a794
Shruti Shaw <shrutishaw750@gmail.com>
2026-06-28 13:54:18 +0530

    Simulate concurrent IoT device data ingestion using thread pool

--- ingestion-service/src/main/java/com/shruti/homeenergy/ingestionservice/IngestionServiceApplication.java
+++ ingestion-service/src/main/java/com/shruti/homeenergy/ingestionservice/IngestionServiceApplication.java
@@ -2,7 +2,9 @@ package com.shruti.homeenergy.ingestionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class IngestionServiceApplication {
 
--- ingestion-service/src/main/java/com/shruti/homeenergy/ingestionservice/dto/EnergyUsageDto.java
+++ ingestion-service/src/main/java/com/shruti/homeenergy/ingestionservice/dto/EnergyUsageDto.java
@@ -1,8 +1,11 @@
 package com.shruti.homeenergy.ingestionservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
 import java.time.Instant;
 
+@Builder
 public record EnergyUsageDto(
         Long deviceId,
         double energyConsumed,
